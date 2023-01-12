537c3ccf9a1766f92007464a6dc77038d2259419
GerardOne <gerardpc78@gmail.com>
2023-01-12 00:29:22 -0600

    cambio en el nav

--- assets/js/carrito.js
+++ assets/js/carrito.js
@@ -1,5 +1,89 @@
 // Carrito
 // #1 BASE DE DATOS
+// const db = [
+//   {
+//     id: 1,
+//     nombre: 'Hamburguesa',
+//     descripcion: 'Hamburguesa de carne de res',
+//     precio: 18.00,
+//     imagen: './assets/img/hamburger.png',
+//     categoria: 'aperitivos',
+//     cantidad: 10
+//   },
+//   {
+//     id: 2,
+//     nombre: 'Papas Fritas',
+//     descripcion: 'Papas fritas de tamaño mediano',
+//     precio: 12.00,
+//     imagen: './assets/img/french-fries.png',
+//     categoria: 'aperitivos',
+//     cantidad: 15
+//   },
+//   {
+//     id: 3,
+//     nombre: 'Hot-Dog',
+//     descripcion: 'Hot-Dog de tamaño mediano',
+//     precio: 16.00,
+//     imagen: './assets/img/hot_dog.png',
+//     categoria: 'aperitivos',
+//     cantidad: 13
+//   },
+//   {
+//     id: 4,
+//     nombre: 'Sandwich',
+//     descripcion: 'Sandwich de jamón y queso',
+//     precio: 15.00,
+//     imagen: './assets/img/sandwich.png',
+//     categoria: 'aperitivos',
+//     cantidad: 16
+//   },
+//   {
+//     id: 5,
+//     nombre: 'Donas',
+//     descripcion: 'Donas de chocolate',
+//     precio: 10.00,
+//     imagen: './assets/img/donut.png',
+//     categoria: 'aperitivos',
+//     cantidad: 24
+//   },
+//   {
+//     id: 6,
+//     nombre: 'Nuguets',
+//     descripcion: 'Nuggets de pollo',
+//     precio: 14.00,
+//     imagen: './assets/img/chicken_leg.png',
+//     categoria: 'aperitivos',
+//     cantidad: 20
+//   },
+//   {
+//     id: 7,
+//     nombre: 'Soda Lata',
+//     descripcion: 'Soda de 355ml',
+//     precio: 13.00,
+//     imagen: './assets/img/soda.png',
+//     categoria: 'bebidas',
+//     cantidad: 14
+//   },
+//   {
+//     id: 8,
+//     nombre: 'Soda Vaso',
+//     descripcion: 'Soda de 500ml',
+//     precio: 15.00,
+//     imagen: './assets/img/soda_cup.png',
+//     categoria: 'bebidas',
+//     cantidad: 18
+//   },
+//   {
+//     id: 9,
+//     nombre: 'Jugo',
+//     descripcion: 'Jugo de 500ml',
+//     precio: 11.00,
+//     imagen: './assets/img/glass.png',
+//     categoria: 'bebidas',
+//     cantidad: 15
+//   }
+// ]
+
 const db = [
   {
     id: 1,
@@ -65,6 +149,9 @@ const db = [
     cantidad: 25
   }
 ]
+
+
+
 
 // #2 Pintar los productos en el DOM
 const productos = db
--- assets/js/data/data.js
+++ assets/js/data/data.js
@@ -1,8 +1,92 @@
+// const db = [
+//   {
+//     id: 1,
+//     nombre: 'Hamburguesa',
+//     descripcion: 'Hamburguesa de carne de res',
+//     precio: 18.00,
+//     imagen: './assets/img/hamburger.png',
+//     categoria: 'aperitivos',
+//     cantidad: 10
+//   },
+//   {
+//     id: 2,
+//     nombre: 'Papas Fritas',
+//     descripcion: 'Papas fritas de tamaño mediano',
+//     precio: 12.00,
+//     imagen: './assets/img/french-fries.png',
+//     categoria: 'aperitivos',
+//     cantidad: 15
+//   },
+//   {
+//     id: 3,
+//     nombre: 'Hot-Dog',
+//     descripcion: 'Hot-Dog de tamaño mediano',
+//     precio: 16.00,
+//     imagen: './assets/img/hot_dog.png',
+//     categoria: 'aperitivos',
+//     cantidad: 13
+//   },
+//   {
+//     id: 4,
+//     nombre: 'Sandwich',
+//     descripcion: 'Sandwich de jamón y queso',
+//     precio: 15.00,
+//     imagen: './assets/img/sandwich.png',
+//     categoria: 'aperitivos',
+//     cantidad: 16
+//   },
+//   {
+//     id: 5,
+//     nombre: 'Donas',
+//     descripcion: 'Donas de chocolate',
+//     precio: 10.00,
+//     imagen: './assets/img/donut.png',
+//     categoria: 'aperitivos',
+//     cantidad: 24
+//   },
+//   {
+//     id: 6,
+//     nombre: 'Nuguets',
+//     descripcion: 'Nuggets de pollo',
+//     precio: 14.00,
+//     imagen: './assets/img/chicken_leg.png',
+//     categoria: 'aperitivos',
+//     cantidad: 20
+//   },
+//   {
+//     id: 7,
+//     nombre: 'Soda Lata',
+//     descripcion: 'Soda de 355ml',
+//     precio: 13.00,
+//     imagen: './assets/img/soda.png',
+//     categoria: 'bebidas',
+//     cantidad: 14
+//   },
+//   {
+//     id: 8,
+//     nombre: 'Soda Vaso',
+//     descripcion: 'Soda de 500ml',
+//     precio: 15.00,
+//     imagen: './assets/img/soda_cup.png',
+//     categoria: 'bebidas',
+//     cantidad: 18
+//   },
+//   {
+//     id: 9,
+//     nombre: 'Jugo',
+//     descripcion: 'Jugo de 500ml',
+//     precio: 11.00,
+//     imagen: './assets/img/glass.png',
+//     categoria: 'bebidas',
+//     cantidad: 15
+//   }
+// ]
+
 const db = [
   {
     id: 1,
-    nombre: 'Monitor',
-    descripcion: 'Monitor Gamming',
+    nombre: 'Monitor Gaming',
+    descripcion: 'Monitor Gaming FHD 22"',
     precio: 250.00,
     imagen: './assets/img/Monitor.png',
     categoria: 'Gamming',
@@ -11,7 +95,7 @@ const db = [
   {
     id: 2,
     nombre: 'Play Station 5',
-    descripcion: 'Consola Play Station 5',
+    descripcion: 'Play Station 5 edición FIFA23',
     precio: 800.00,
     imagen: './assets/img/Playstation.png',
     categoria: 'Gamming',
@@ -20,7 +104,7 @@ const db = [
   {
     id: 3,
     nombre: 'Laptop',
-    descripcion: 'Laptop Gamming',
+    descripcion: 'Laptop Gaming',
     precio: 750.00,
     imagen: './assets/img/laptop.png',
     categoria: 'Gamming',
@@ -29,7 +113,7 @@ const db = [
   {
     id: 4,
     nombre: 'Silla de Escritorio',
-    descripcion: 'Silla de Escritorio Gamming',
+    descripcion: 'Silla de Escritorio Gaming',
     precio: 250.00,
     imagen: './assets/img/silla.png',
     categoria: 'Gamming',
@@ -38,7 +122,7 @@ const db = [
   {
     id: 5,
     nombre: 'Mouse',
-    descripcion: 'Mouse Gamming',
+    descripcion: 'Mouse Gaming',
     precio: 100.00,
     imagen: './assets/img/mouse.png',
     categoria: 'Gamming',
@@ -47,16 +131,16 @@ const db = [
   {
     id: 6,
     nombre: 'Audifonos',
-    descripcion: 'Audifonos Gamming',
+    descripcion: 'Audifonos Gaming',
     precio: 125.00,
-    imagen: './assets/img/audifonos.png',
+    imagen: 'assets/img/audífonos.png',
     categoria: 'Gamming',
     cantidad: 40
   },
   {
     id: 7,
-    nombre: 'Teclado',
-    descripcion: 'Teclado Gamming',
+    nombre: 'Teclado RGB',
+    descripcion: 'Teclado Gaming RGB color arcoiris',
     precio: 90.00,
     imagen: './assets/img/teclado.png',
     categoria: 'Gamming',
